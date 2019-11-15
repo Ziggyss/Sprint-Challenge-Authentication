28fac57b15f66e7dd79b05b9e1d25f40987967fa
Lisa Wilton <lisawilton@Lisas-Air.fritz.box>
2019-11-15 10:26:28 +0000

    all tests passing

--- auth/auth.spec.js
+++ auth/auth.spec.js
@@ -7,14 +7,24 @@ describe("register endpoint", () => {
   beforeEach(async () => {
     await db("users").truncate();
   });
-  it("returns a 200 status", () => {
+  it("returns a 201 status with the right data", () => {
     request(server)
       .post("/api/auth/register")
       .send({ username: "Lisa", password: "1234" })
       .then(res => {
         expect(res.status)
-          .toBe(200)
+          .toBe(201)
           .expect("Content-Type", /json/);
+      });
+  });
+  it("returns the new user", () => {
+    request(server)
+      .post("/api/auth/register")
+      .send({ username: "Zuri", password: "1234" })
+      .then(res => {
+        expect(res).toContain({
+          username: "Zuri"
+        });
       });
   });
 });
@@ -30,6 +40,16 @@ describe("login endpoint", () => {
           .expect("Content-Type", /json/);
       });
   });
+  it("returns a welcome message on login", () => {
+    request(server)
+      .post("/api/auth/login")
+      .send({ username: "Zuri", password: "1234" })
+      .then(res => {
+        expect(res).toContain({
+          message: "Welcome Zuri!"
+        });
+      });
+  });
 });
 
 describe("jokes endpoint", () => {
@@ -43,7 +63,7 @@ describe("jokes endpoint", () => {
           .expect("Content-Type", /json/);
       });
   });
-  it("returns a 400 status with missing credentials", () => {
+  it("returns a 401 status with missing credentials", () => {
     request(server)
       .get("api/jokes")
       .send({ username: "", password: "" })
